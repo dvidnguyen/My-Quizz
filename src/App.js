9d33f607b02105dee781932b7d007e6d63efc374
=dvidnguyen <=qndjxz@gmail.com>
2025-04-07 11:17:15 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,62 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
-import Menu from './Menu';
-import List from './Component/List';
+import './App.scss';
 import { useState } from 'react';
+import Header from './Component/Header/Header';
+import {Link} from "react-router-dom";
 function App() {
-  const [state,setState] = useState({
-    name : "dvid ngurn",
-    address :"duong so 3",
-    age:20,
-});
-  function handleOver(event){
-    console.log(event.pageX)
-  }
-  const handleOnchangeIput = (event)=>{
-    setState({
-      ...state, name : event.target.value
-    })
-  }
-  const handleAddNewUser=()=>{}
-  const handleSubmit =(event)=>{
-    event.preventDefault();
-    
-    
-  }
-   const [user, setUser] = useState([
-      {
-        id: 1,
-        name: "dvidnguyen",
-        age: 30,
-      },
-      {
-        id: 2,
-        name: "thuhuong",
-        age: 27,
-      },
-      {
-        id: 3,
-        name: "minhkhoi",
-        age: 32,
-      },
-    ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} style={{height :"100px" , width :"100px"}} className="App-logo" alt="logo" />
-
-        <p>{state.name}</p>
-        <button onMouseOver={handleOver} >Click me </button>
-        <form onSubmit={handleSubmit}>
-          <input value={state.name} type="text" onChange={handleOnchangeIput} />
-          <button>Submit</button>
-        </form>
-        <List user={user} 
-
-        />
-      </header>
+  <div className='app-container'>
+    <Header/>
+    <div>
+      test link 
+      <div>
+          <button>
+            <Link to="/users" >go to user page</Link>
+          </button>
+          <button>   
+          <Link to="/admins" >go to admin page</Link>
+          </button>
+      </div>
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
